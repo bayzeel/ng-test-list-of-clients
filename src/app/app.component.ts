1a93b69d4b22e74a21452dcc7a4fa44ffbba1c93
Basil Sklepkovich <vsklepkovich@gmail.com>
2017-07-08 22:00:02 +0300

    Add search

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,21 +1,15 @@
 import { Component, OnInit } from '@angular/core';
 
-import { CommonService } from './list/common.service';
-
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent implements OnInit{
-  clients: any;
 
-  constructor(private commonService : CommonService){}
+  constructor(){}
 
   ngOnInit() {
-    this.commonService.getAllRecords().then((res) => {
-      console.log(res);
-      this.clients = res;
-    });
+
   }
 }
--- src/app/list/list.component.ts
+++ src/app/list/list.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+import { CommonService } from './common.service';
 
 @Component({
   selector: 'app-list',
@@ -6,11 +8,50 @@ import { Component, OnInit, Input } from '@angular/core';
   styleUrls: ['./list.component.css']
 })
 export class ListComponent implements OnInit {
-  @Input() clients;
+  clients: any;
+  clientsFull: any;
+  arrLength: number;
 
-  constructor() { }
+  typingTimer: any;
+
+  constructor(private commonService : CommonService) { }
+
+  search(term: string): void {
+    // angular's debounceTime() alternative
+    clearTimeout(this.typingTimer);
+    this.typingTimer = setTimeout(() => {
+      let tempArr = [];
+
+      term = term.toLowerCase();
+
+      // deep search
+      outer:
+        for(let i = 0; i <= this.arrLength; i++){
+          let parentObj = this.clientsFull[i];
+          for(let index in parentObj){
+            if(typeof parentObj[index] === 'object'){
+              let childObj = parentObj[index];
+              for(let idx in childObj){
+                let objVal = String(childObj[idx]).toLowerCase();
+                if(idx !== 'avatar' && objVal.indexOf(term) !== -1){
+                  tempArr.push(this.clientsFull[i]);
+                  continue outer;
+                }
+              }
+            }
+          }
+        }
+      this.clients = tempArr.slice();
+      tempArr.length = 0;
+    }, 300);
+  }
 
   ngOnInit() {
+    this.commonService.getAllRecords().then((res) => {
+      this.clients = res;
+      this.clientsFull = res;
+      this.arrLength = res.length;
+    });
   }
 
 }
